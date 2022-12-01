ef8d2e9481c3001f425b5627a1a1c2dcea65ad39
Jakub Chaba <jakub.chaba@btopenworld.com>
2022-12-01 16:07:05 -0500

    Solution for day 1

---
+++ 1/advent-1.py
@@ -0,0 +1,37 @@
+class Elf: 
+	def __init__(self, number):
+		self.number = number
+		self.calories = []
+
+	def appendCalories(self, kcal):
+		self.calories.append(kcal)
+	
+	def getTotalCalories(self):
+		return sum(self.calories)
+	
+	def getElfName(self):
+		return "Elf " + str(self.number)
+
+elfDictionary = dict()
+elfNumber = 1
+elf = Elf(elfNumber)
+with open("input.txt", 'r') as file:
+	lines = file.readlines()
+
+	for line in lines:
+		if line.strip():
+			elf.appendCalories(int(line))
+		else:
+			elfDictionary[elf.getElfName()] = elf.getTotalCalories()
+			elfNumber = elfNumber + 1
+			elf = Elf(elfNumber)
+
+sumOfThreeFattest = 0
+
+for i in range(3):
+	fatElf = max(elfDictionary, key=elfDictionary.get)	
+	print("Fattest Elf: " + str(fatElf) + " with " + str(elfDictionary[fatElf]) + " kcal")
+	sumOfThreeFattest = sumOfThreeFattest + elfDictionary[fatElf]
+	del elfDictionary[fatElf]
+
+print("Calories carried by top 3 elves: " + str(sumOfThreeFattest))
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+
+My submissions for [Advent of Code](https://adventofcode.com/2022/about).
