ddc46bb89e1bc11c6608f21ba011be1ebb3c40ac
Jakub Chaba <jakub.chaba@btopenworld.com>
2022-12-06 16:06:16 -0500

    Solution for day 6

--- 6/advent-6.py
+++ 6/advent-6.py
@@ -7,14 +7,13 @@ with open("input.txt", 'r') as file:
 	for line in lines:
 		length = len(line)
 		for i in range(length):
-			if (i < length - 4):
-				packetWindow = line[i:i+4]
-				messageWindow = line[i:i+14]
-				packetFreq = Counter(packetWindow)
-				messageFreq = Counter(messageWindow)
-				if (len(packetFreq) == 4 and not detectedPacket):
-					print("Part 1: ", i + 4)
-					detectedPacket = True
-				if (len(messageFreq) == 14 and not detectedMessage):
-					print("Part 2: ", i + 14)
-					detectedMessage = True
+			packetWindow = line[i:i+4]
+			messageWindow = line[i:i+14]
+			packetFreq = Counter(packetWindow)
+			messageFreq = Counter(messageWindow)
+			if (len(packetFreq) == 4 and not detectedPacket):
+				print("Part 1: ", i + 4)
+				detectedPacket = True
+			if (len(messageFreq) == 14 and not detectedMessage):
+				print("Part 2: ", i + 14)
+				detectedMessage = True
